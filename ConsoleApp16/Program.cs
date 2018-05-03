f7b4b04a85db50bf80fb2d0830fc9bfafb51aa5f
SilverBarnabas <silverbarnabas@hot.ee>
2018-05-03 09:20:49 +0300

    uyji

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ConsoleApp16
 {
@@ -6,7 +9,25 @@ namespace ConsoleApp16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wormkmnjmnmnmnld!");
+            var paljuTooteid = new List<Toode>();
+            var toode1 = new Toode() { Nimi = "Banaan", Hind = "12" };
+            var toode2 = new Toode() { Nimi = "Leib", Hind = "14" };
+            var toode3 = new Toode() { Nimi = "Sai", Hind = "16" };
+            var toode4 = new Toode() { Nimi = "Mahl", Hind = "17" };
+            var toode5 = new Toode() { Nimi = "Liha", Hind = "77" };
+
+            paljuTooteid.Add(toode1);
+            paljuTooteid.Add(toode2);
+            paljuTooteid.Add(toode3);
+            paljuTooteid.Add(toode4);
+            paljuTooteid.Add(toode5);
+
+            var serializer = new XmlSerializer(paljuTooteid.GetType());
+            using(var writer = XmlWriter.Create("tooted.xml"))
+            {
+                serializer.Serialize(writer, paljuTooteid);
+            }
+
         }
     }
 }
---
+++ ConsoleApp16/Toode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp16
+{
+    class Toode
+    {
+        public string Nimi { get; set; }
+        public string Hind { get; set; }
+
+    }
+}
